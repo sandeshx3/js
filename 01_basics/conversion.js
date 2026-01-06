876b4c241c86c64dcc56200d1bc60608ad077f88
sandeshx3 <b524nrx9fy@privaterelay.appleid.com>
2026-01-06 16:06:56 +0000

    add

---
+++ 01_basics/conversion.js
@@ -0,0 +1,24 @@
+// let score = 15
+// let valueInNumber = Number(score)
+// console.log(typeof score)
+
+
+// '33' => 33
+// '33abc'  => N
+
+// let isLoggedIn = 1 
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log( booleanIsLoggedIn)
+
+
+// 1=> true
+//0 => false
+// 'sam' => true
+
+
+let  sNumber = 3
+let stringNumber = String(sNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+// number can be change into the string
