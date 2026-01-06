43cff334a7887e42026029bc98fb314c7602ec21
sandeshx3 <b524nrx9fy@privaterelay.appleid.com>
2026-01-06 16:57:34 +0000

    add

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -17,8 +17,26 @@
 // 'sam' => true
 
 
-let  sNumber = 3
-let stringNumber = String(sNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let  sNumber = 3
+// let stringNumber = String(sNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
 // number can be change into the string
+
+
+
+//************************OPERATION****************
+// let str1= 'hello'
+// let str2 = ' sandesh'
+// let str3 = str1 + str2
+// console.log(str3)
+
+
+// console.log('1'+2)
+// console.log(1+2+'4')  //op-> 34
+// console.log('1'+2+3) // op-> 123
+
+console.log('2'==2)// this will give the true as conversion will be done 
+console.log('2'=== 2)// this will give the false as this is strict check as both are not the same data type so comparison result in the false
+
+
--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -16,4 +16,17 @@
 
 // object
 // 
-console.log(typeof null)//null datatype is object 
+// console.log(typeof null)//null datatype is object 
+
+
+/// types of datatypes on the basis of how the data are put into memory and and can be acessed
+// 1.primitive(call by value): 7(String,Number,Boolean,null,undefined,Symbol,BigInt)
+// 2. Reference/non-primitive type
+// reference can be directly allocated into memory
+// Array,Object,Functions
+ 
+// const id =Symbol('123')
+// const aId = Symbol('123')
+
+// console.log(id===aId)
+// BigInt=123444n // n is represent in the last
