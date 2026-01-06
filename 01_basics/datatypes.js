7d89aa7b375ac5c55905ed342af9a2fba3361d98
sandeshx3 <b524nrx9fy@privaterelay.appleid.com>
2026-01-06 17:17:41 +0000

    add

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -30,3 +30,33 @@
 
 // console.log(id===aId)
 // BigInt=123444n // n is represent in the last
+
+
+//++++++++++++++++++++++ memory in js ++++++++++
+// stack (primitive type are stored )
+// heap (non primitive type are stored)
+// a copy value can get from the primitive datatype
+// memory original value/reference value can be get from the non primitive datatype
+
+
+
+// let myName = 'sandesh'
+// let anotherName = myName
+
+// anotherName = 'sachin'
+
+// console.log(myName)
+// console.log(anotherName)
+
+
+
+
+let userOne=
+{
+name:'sandesh', 
+email:"abc@mail.com"
+}
+let userTwo = userOne
+userTwo.email=('defA@gmail.com')
+console.log(userOne.email)
+console.log(userTwo.email)
